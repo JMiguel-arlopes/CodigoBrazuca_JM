6648e103f4f1114ba3f9970ba6e3c0dab099498a
JM-arlopes <jmiguelarlopes@gmail.com>
2023-08-25 23:48:40 -0300

    inserindo algoritmo que arredonda números para inteiros

---
+++ exercicios/arredondar-inteiros.py
@@ -0,0 +1,8 @@
+def main():
+    num = float(input('Digite o número ao qual deseja arredondar: '))
+    arredonda = round(num) 
+    print('------------------------------------------------------------------------------')
+    print(f'O número que era {num}, se tornou em sua forma inteira o Número {arredonda}')
+    print('------------------------------------------------------------------------------')
+    
+main()
