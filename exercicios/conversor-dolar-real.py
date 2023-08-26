98618dff2aa1b24ac873e8c4cf3ac06b0a86e95d
JM-arlopes <jmiguelarlopes@gmail.com>
2023-08-25 23:51:37 -0300

    inserindo algoritmo que converte de USD para BRL com API

--- exercicios/conversor-dolar-real.py
+++ exercicios/conversor-dolar-real.py
@@ -1,11 +1,15 @@
 import requests
 
-response = requests.get('http://economia.awesomeapi.com.br/json/last/USD-BRL')
-data = response.json()
-cotacao = float(data["USDBRL"]["bid"])
-print(cotacao)
+def main():
+    quantia = float(input('Quando em dólares você deseja converter para real? '))
+    cotacao = api()
+    print(f"a Conversão de {quantia} USD é equivalente a {round(quantia * cotacao, 2)}R$")
 
-quantia = float(input('Quando em dólares você deseja converter para real? '))
 
-print(f"a Conversão de {quantia} USD é equivalente a {quantia * cotacao}R$")
+def api():
+    response = requests.get('http://economia.awesomeapi.com.br/json/last/USD-BRL')
+    data =  response.json()
+    cotacao = float(data["USDBRL"]["bid"])
+    return cotacao
 
+main()
