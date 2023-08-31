7b6cf23e1306282b05572a2fdd3c1f113e2dc3ac
JM-arlopes <jmiguelarlopes@gmail.com>
2023-08-30 23:52:44 -0300

    inserindo problema 2

---
+++ exercicios/hard/ex-2.py
@@ -0,0 +1,4 @@
+# Crie uma classe Shape com um método abstrato calculate_area. Em seguida, defina
+# Classes derivadas Circle e Triangle que herdam de Shape e implemetam esse método para calcular suas respectivas áreas
+# 
+# 
