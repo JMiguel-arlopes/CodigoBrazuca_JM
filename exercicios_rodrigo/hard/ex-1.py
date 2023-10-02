4ab835f2bcd5c0567bb8dbe219648c7c738e27bb
JM-arlopes <jmiguelarlopes@gmail.com>
2023-10-02 17:21:31 -0300

    organizando pastas

--- exercicios_rodrigo/hard/ex-1.py
+++ exercicios_rodrigo/hard/ex-1.py
@@ -1,2 +1,6 @@
 # Desenvolva uma Class Bank que gerencia várias contas bancárias. Os métodos devem permitir novas contas
 # fazer transferencias entre contas e calcular o saldo total do banco.
+
+
+class Bank:
+    
